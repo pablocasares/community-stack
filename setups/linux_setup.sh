dfbe00823918197a98fd41cc8519993459c56e7e
Andrés Gómez <agomez@wizzie.io>
2018-09-11 10:00:57 +0200

    Set release at linux_setip.sh

--- setups/linux_setup.sh
+++ setups/linux_setup.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-declare -r WCS_VERSION=master
+declare -r WCS_VERSION=1.0.0
 
 . /etc/os-release
 
